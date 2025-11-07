76904a4d3cf6d067df1ed14862be20ad70bac7ec
Kevin Pedro <kpedro88@gmail.com>
2025-11-07 10:34:43 -0600

    setup subsystem

--- setup.sh
+++ setup.sh
@@ -6,9 +6,16 @@ cd ${BUILD_DIR}
 cmsrel ${CMSSW_VERSION}
 cd ${CMSSW_VERSION}/src
 cmsenv
+
 git cms-init
 git remote add c4h git@github.com:code4hep/cmssw
+git checkout -b code4hep c4h/code4hep
+git cms-addpkg Code4hep
+
 mkdir -p ${CMSSW_BASE}/build
 mkdir -p ${CMSSW_BASE}/install
 cd ${CMSSW_BASE}/build
 ${SETUP_DIR}/install.sh
+
+cd ${CMSSW_BASE}/src
+scram b -j ${C4H_BUILD_CORES}

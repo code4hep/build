a3ebc424651e2eb9bbd20c429b896165eab04fdd
Kevin Pedro <kpedro88@gmail.com>
2025-11-19 10:17:59 -0600

    install test script

--- install/edm4hep.sh
+++ install/edm4hep.sh
@@ -6,14 +6,19 @@ cd EDM4hep
 mkdir build_edm4hep
 cd build_edm4hep
 
+EDM4HEP_PREFIX=${CMSSW_BASE}/install/edm4hep
 cmake ../ \
-  -DCMAKE_INSTALL_PREFIX=${CMSSW_BASE}/install/edm4hep \
+  -DCMAKE_INSTALL_PREFIX=${EDM4HEP_PREFIX} \
   -DROOT_DIR="$(scram tool tag root_interface ROOT_INTERFACE_BASE)"/cmake \
   -DEDM4HEP_WITH_JSON=OFF \
   -DUSE_EXTERNAL_CATCH2=OFF
 
 make -j ${C4H_BUILD_CORES}
 make install
+
+# manually install test script
+mkdir -p ${EDM4HEP_PREFIX}/bin
+cp ../scripts/createEDM4hepFile.py ${EDM4HEP_PREFIX}/bin
 
 # scram
 cat << EOF_TOOLFILE > edm4hep.xml
@@ -27,9 +32,9 @@ cat << EOF_TOOLFILE > edm4hep.xml
   </client>
   <runtime name="ROOT_INCLUDE_PATH" value="\$INCLUDE" type="path"/>
   <runtime name="PYTHON3PATH" default="\$EDM4HEP_BASE/lib/python3.9/site-packages" type="path"/>
+  <runtime name="PATH" default="\$EDM4HEP_BASE/bin" type="path"/>
 </tool>
 EOF_TOOLFILE
 
 mv edm4hep.xml ${CMSSW_BASE}/config/toolbox/${SCRAM_ARCH}/tools/selected/
 scram setup edm4hep
-

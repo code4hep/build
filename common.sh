4ea6ada0c443bd78d8fc3d9d60e8a697e85272d2
Kevin Pedro <kpedro88@gmail.com>
2026-01-15 15:31:47 -0600

    update version

--- common.sh
+++ common.sh
@@ -2,6 +2,6 @@
 
 export SETUP_DIR=$(readlink -f $(dirname -- "${BASH_SOURCE[0]}"))
 export BUILD_DIR=$(readlink -f "$SETUP_DIR"/..)
-export CMSSW_VERSION=CMSSW_16_0_0_pre2
+export CMSSW_VERSION=CMSSW_16_1_0_pre1
 export CMSSW_DIR=${BUILD_DIR}/${CMSSW_VERSION}
 export C4H_BUILD_CORES=8

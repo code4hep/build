0e0b56cfe9dd2341d488394ad68b51617c3c1804
Kevin Pedro <kpedro88@gmail.com>
2026-07-01 11:28:26 -0500

    try to remove CLHEP patch

--- install/code4hep.sh
+++ install/code4hep.sh
@@ -7,41 +7,6 @@ cd Code4hep
 mkdir build_Code4hep
 cd build_Code4hep
 
-cat << 'EOF' > clhep_patch.cmake
-# Guard variable to prevent infinite recursion loops
-set(_CLHEP_IN_FIND_PACKAGE FALSE CACHE INTERNAL "Global find_package execution fence")
-
-macro(find_package _package)
-  message(STATUS "[PATCH] starting patched find_package for ${_package}")
-  if(_CLHEP_IN_FIND_PACKAGE)
-    # We are already inside our custom wrapper, forward directly to CMake's engine
-    message(STATUS "[PATCH] forwarding to CMake")
-    _find_package(${ARGV})
-  else()
-    message(STATUS "[PATCH] getting ready to override")
-    # Raise the execution fence
-    set(_CLHEP_IN_FIND_PACKAGE TRUE CACHE INTERNAL "")
-    # Run the native CMake find_package engine
-    message(STATUS "[PATCH] run native CMake find_package")
-    _find_package(${ARGV})
-    # Force override the variable if it points to the broken CMSSW path
-    if("${_package}" STREQUAL "CLHEP" OR CLHEP_INCLUDE_DIR)
-      set(_CORRECT_CLHEP "MY_CLHEP_PATH")
-      if(CLHEP_INCLUDE_DIR MATCHES "CMSSW")
-        message(STATUS "[PATCH] Overriding broken CLHEP path: ${CLHEP_INCLUDE_DIR}")
-        message(STATUS "[PATCH] Fixed CLHEP path: ${_CORRECT_CLHEP}")
-        set(CLHEP_INCLUDE_DIR "${_CORRECT_CLHEP}" PARENT_SCOPE)
-        set(CLHEP_INCLUDE_DIR "${_CORRECT_CLHEP}")
-        set(CLHEP_INCLUDE_DIR "${_CORRECT_CLHEP}" CACHE PATH "Force corrected CLHEP include path" FORCE)
-      endif()
-    endif()
-    # Lower the execution fence for the next package search
-    set(_CLHEP_IN_FIND_PACKAGE FALSE CACHE INTERNAL "")
-  endif()
-endmacro()
-EOF
-sed -i 's~MY_CLHEP_PATH~'$(scram_tag clhep CLHEP_BASE)/include'~g' clhep_patch.cmake
-
 CODE4HEP_PREFIX=${INSTALL_DIR}/code4hep
 CMAKE_PREFIXES=(
 $(scram_tag boost BOOST_BASE) \
@@ -60,7 +25,6 @@ cmake ../ \
   -Wno-dev \
   -DCMAKE_INSTALL_PREFIX=${CODE4HEP_PREFIX} \
   -DCMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE" \
-  -DCMAKE_PROJECT_Code4hep_INCLUDE=build_Code4hep/clhep_patch.cmake \
   -DCMAKE_PREFIX_PATH=$(join_path "${CMAKE_PREFIXES[@]}") \
   -DPython3_ROOT_DIR=$(scram_tag python3 PYTHON3_BASE) \
   -DCLHEP_ROOT="$(find $(scram_tag clhep LIBDIR) -maxdepth 1 -type d -name "CLHEP*" -print -quit)" \

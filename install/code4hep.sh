883a45a67b678c1aace9075627e304b2d5ec086f
Kevin Pedro <kpedro88@gmail.com>
2026-07-31 12:57:29 -0500

    update c4h dependencies

--- install/code4hep.sh
+++ install/code4hep.sh
@@ -7,6 +7,17 @@ cd Code4hep
 mkdir build_Code4hep
 cd build_Code4hep
 
+mkdir -p cmake_patch
+cat << 'EOF' > cmake_patch/Pythia8Config.cmake
+set(Pythia8_FOUND TRUE)
+message(STATUS "[PATCH] Creating synthetic Pythia8 target from ${Pythia8_LIBRARY} and ${Pythia8_INCLUDE_DIR}")
+add_library(Pythia8::Pythia8 UNKNOWN IMPORTED)
+set_target_properties(Pythia8::Pythia8 PROPERTIES
+	IMPORTED_LOCATION "${Pythia8_LIBRARY}"
+	INTERFACE_INCLUDE_DIRECTORIES "${Pythia8_INCLUDE_DIR}"
+)
+EOF
+
 CODE4HEP_PREFIX=${INSTALL_DIR}/code4hep
 CMAKE_PREFIXES=(
 $(scram_tag boost BOOST_BASE) \
@@ -16,15 +27,20 @@ $(scram_tag json JSON_BASE) \
 $(scram_tag catch2 CATCH2_BASE) \
 $(scram_tag dd4hep-core DD4HEP_CORE_BASE) \
 $(scram_tag py3-pybind11 PY3_PYBIND11_BASE) \
+$(scram_tag hepmc3 HEPMC3_BASE) \
+${PWD}/cmake_patch \
 ${INSTALL_DIR}/c4h_md5 \
 ${INSTALL_DIR}/podio \
 ${INSTALL_DIR}/edm4hep \
 ${INSTALL_DIR}/stitched \
 )
+
 cmake ../ \
   -Wno-dev \
   -DCMAKE_INSTALL_PREFIX=${CODE4HEP_PREFIX} \
   -DCMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE" \
+  -DPythia8_INCLUDE_DIR=$(scram_tag pythia8 INCLUDE) \
+  -DPythia8_LIBRARY=$(scram_tag pythia8 LIBDIR)/libpythia8.so \
   -DCMAKE_PREFIX_PATH=$(join_path "${CMAKE_PREFIXES[@]}") \
   -DPython3_ROOT_DIR=$(scram_tag python3 PYTHON3_BASE) \
   -DCLHEP_ROOT="$(find $(scram_tag clhep LIBDIR) -maxdepth 1 -type d -name "CLHEP*" -print -quit)" \

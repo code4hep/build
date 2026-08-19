65d90e60db89e9cbd6ff801778f33864dc380b67
Kevin Pedro <kpedro88@gmail.com>
2026-08-19 17:04:45 -0500

    simplify patch

--- install/code4hep.sh
+++ install/code4hep.sh
@@ -26,20 +26,13 @@ include("MY_GEANT4_DIR/Geant4Config.cmake")
 
 # 2. Create the global target using the newly loaded variables
 if(Geant4_FOUND AND NOT TARGET Geant4::Geant4)
-    message(STATUS "[Proxy] Creating global target Geant4::Geant4 from \${Geant4_LIBRARIES}")
+    message(STATUS "[Proxy] Creating global target Geant4::Geant4 from \${Geant4_LIBRARIES} as IMPORTED")
 
-    # Step A: Create a local interface library
-    add_library(Geant4_wrapper_target INTERFACE)
+    # By defining it as INTERFACE IMPORTED GLOBAL, CMake treats it like a system target
+    add_library(Geant4::Geant4 INTERFACE IMPORTED GLOBAL)
 
-    # Step B: Link the dynamic libraries to our local wrapper
-    # Hide the wrapper dependency from public export sets
-    target_link_libraries(Geant4_wrapper_target INTERFACE
-        $<BUILD_INTERFACE:${Geant4_LIBRARIES}>
-    )
-
-    # Step C: Create the ALIAS target
-    # This target is automatically visible globally across the project.
-    add_library(Geant4::Geant4 ALIAS Geant4_wrapper_target)
+    # Directly link the dynamic Geant4 libraries to it
+    target_link_libraries(Geant4::Geant4 INTERFACE ${Geant4_LIBRARIES})
 endif()
 EOF
 sed -i 's~MY_GEANT4_DIR~'${INSTALL_DIR}/geant4/lib/cmake/Geant4'~' ${PATCH_DIR}/Geant4Config.cmake
